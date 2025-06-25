b5c7479884146ce129265ebc4f002bc4bbc9b3cc
Chew Teong Han <teonghan@gmail.com>
2025-06-26 07:19:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ def main():
     with tabs[1]:
         if df_export_with_asjc is not None:
             section_map_export_csv(df_export_with_asjc, df_asjc)
-            section_author_analysis(df_export_with_asjc)
+            section_author_asjc_summary(df_export_with_asjc)  # <-- use correct function name!
         else:
             st.info("Please upload both the Scopus Source Excel and Export CSV(s) to use this section.")
 
